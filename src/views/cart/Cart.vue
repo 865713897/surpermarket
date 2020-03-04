ae3c81930f921f475a6df68c6ff65f7ca2b14026
865713897@qq.com <865713897@qq.com>
2020-03-04 16:41:45 +0800

    '提交项目'

---
+++ src/views/cart/Cart.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="">
+    <h2>购物车</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/views/profile/Profile.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="">
+    <h2>个人</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+}
+</script>
+<style scoped>
+
+</style>
